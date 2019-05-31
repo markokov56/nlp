ca67b7d2f05c88424002ebfeecf565c4d303d84e
markokov56 <42272472+markokov56@users.noreply.github.com>
2019-05-31 14:30:03 +0200

    Update apitwitter_git.py

--- apitwitter_git.py
+++ apitwitter_git.py
@@ -2,8 +2,8 @@ import requests
 print("requests package: " + requests.__version__))
 import json
 print("json package: " + json.__version__))
-import base64
-print("base64 package: " + base64.__version__))
+#import base64
+#print("base64 package: " + base64.__version__))
 
 print("end py run")
 
